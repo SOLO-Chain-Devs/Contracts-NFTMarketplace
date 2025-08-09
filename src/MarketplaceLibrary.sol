2c10e778222c1c4060d402782ddd2041edfe545c
The Dip <thedip@jeet.com>
2025-08-08 21:07:31 -0600

    added view functions and changed royalty logic added tests

--- src/MarketplaceLibrary.sol
+++ src/MarketplaceLibrary.sol
@@ -3,11 +3,13 @@
 pragma solidity ^0.8.27;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 import "./interface/IERC2981.sol";
 import "./interface/IERC6909.sol";
 
 library MarketplaceLibrary {
+    using SafeERC20 for IERC20;
     function handleBidPayment(address _currency, uint256 _amount) internal returns (uint256) {
         if (_currency == address(0)) {
             if (msg.value == 0) revert InsufficientPayment();
@@ -16,18 +18,17 @@ library MarketplaceLibrary {
             if (_amount == 0) revert TokenAmountMustBeGreaterThanZero();
             if (msg.value != 0) revert EthNotAccepted();
             IERC20 token = IERC20(_currency);
-            if (!token.transferFrom(msg.sender, address(this), _amount)) {
-                revert TokenTransferFailed();
-            }
+            // SafeERC20 handles non-standard ERC20s that return no boolean
+            token.safeTransferFrom(msg.sender, address(this), _amount);
             return _amount;
         }
     }
 
     function transferPaymentToAddress(address _currency, uint256 _amount, address _recipient) internal {
         if (_currency == address(0)) {
-            payable(_recipient).transfer(_amount);
+            Address.sendValue(payable(_recipient), _amount);
         } else {
-            IERC20(_currency).transfer(_recipient, _amount);
+            IERC20(_currency).safeTransfer(_recipient, _amount);
         }
     }
 
@@ -35,7 +36,7 @@ library MarketplaceLibrary {
         address _tokenAddress
     ) internal view returns (bool) {
         if (_isContract(_tokenAddress)) {
-            return IERC165(_tokenAddress).supportsInterface(0x80ac58cd);
+            return _supportsInterface(_tokenAddress, 0x80ac58cd);
         }
         return false;
     }
@@ -44,7 +45,7 @@ library MarketplaceLibrary {
         address _tokenAddress
     ) internal view returns (bool) {
         if (_isContract(_tokenAddress)) {
-            return IERC165(_tokenAddress).supportsInterface(0xd9b67a26);
+            return _supportsInterface(_tokenAddress, 0xd9b67a26);
         }
         return false;
     }
@@ -53,7 +54,7 @@ library MarketplaceLibrary {
         address _tokenAddress
     ) internal view returns (bool) {
         if (_isContract(_tokenAddress)) {
-            return IERC2981(_tokenAddress).supportsInterface(0x2a55205a);
+            return _supportsInterface(_tokenAddress, 0x2a55205a);
         }
         return false;
     }
@@ -62,7 +63,7 @@ library MarketplaceLibrary {
         address _tokenAddress
     ) internal view returns (bool) {
         if (_isContract(_tokenAddress)) {
-            return IERC165(_tokenAddress).supportsInterface(0x0f632fb3);
+            return _supportsInterface(_tokenAddress, 0x0f632fb3);
         }
         return false;
     }
@@ -77,13 +78,21 @@ library MarketplaceLibrary {
         return size > 0;
     }
 
+    function _supportsInterface(address target, bytes4 interfaceId) internal view returns (bool) {
+        // Perform a safe staticcall to supportsInterface(bytes4) and decode
+        (bool ok, bytes memory data) = target.staticcall(abi.encodeWithSelector(0x01ffc9a7, interfaceId));
+        if (!ok || data.length < 32) {
+            return false;
+        }
+        return abi.decode(data, (bool));
+    }
+
     /**
-     * @notice Handles the payment of royalties for a sale
+     * @notice Handles the payment of royalties for a sale per EIP-2981 (salePrice is total for the sale)
      * @param _tokenAddress The NFT contract address
      * @param _tokenId The NFT token ID
-     * @param _salePrice The sale price
+     * @param _salePrice The total sale price (for multiples, pass unitPrice * quantity)
      * @param _currency The currency used for payment
-     * @param _amount The amount of tokens (for ERC1155)
      * @return netAmount The amount after deducting royalties
      */
     function handleRoyalties(
@@ -91,15 +100,10 @@ library MarketplaceLibrary {
         uint256 _tokenId,
         uint256 _salePrice,
         address _currency,
-        uint256 _amount
+        uint256 /* _amount */
     ) internal returns (uint256) {
-        // We know royalties exist and are valid because we checked in getRoyaltyInfo
+        // Royalty per EIP-2981: salePrice is the TOTAL price of the sale
         (address receiver, uint256 royaltyAmount) = IERC2981(_tokenAddress).royaltyInfo(_tokenId, _salePrice);
-
-        // If it's ERC1155 or ERC6909, multiply royalty by quantity
-        if (isERC1155(_tokenAddress) || isERC6909(_tokenAddress)) {
-            royaltyAmount = royaltyAmount * _amount;
-        }
 
         if (royaltyAmount > 0) {
             if (royaltyAmount >= _salePrice) {
